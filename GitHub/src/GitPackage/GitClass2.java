a8fe8059845b23f3ac8dbb16896b9be1b9b3c06f
DELL <DELL@DESKTOP-DIQ1OMU>
2022-10-29 19:48:04 +0530

    class 2 updated

--- GitHub/src/GitPackage/GitClass2.java
+++ GitHub/src/GitPackage/GitClass2.java
@@ -3,7 +3,7 @@ package GitPackage;
 public class GitClass2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Naresh tho mamulga undadhu");
 
 	}
 
